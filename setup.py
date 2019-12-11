7203ec28e5690ee596901d828e465267bd1df6a2
Ray A. Matsumoto <ray.a.matsumoto@vanderbilt.edu>
2019-12-11 13:50:26 -0600

    Add setup.py for cookiecutter

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+        name="foyer-cookiecutter",
+        version="0.0.0",
+        description="A Cookiecutter template for creating foyer recipes",
+        author="Ray Matsumoto",
+        author_email="ray.a.matsumoto@vanderbilt.edu"
+    )
